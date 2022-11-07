274f031b225a9707bc836deb212af8bf68c459e6
Sagar-Patel94 <s.m.gajera.sg@gmail.com>
2022-11-07 12:19:28 +0530

    07/11/2022 Monday 12:19 PM

---
+++ tribe_sso/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "hacker-header": "#ff6600",
+        "hacker-body": "#f6f6ef",
+      },
+    },
+  },
+  plugins: [],
+};
